833d837216ad7ca1b8f15c24398c9d5f9d69bddf
Yashmevada2807 <yashmevada07@gmail.com>
2025-08-21 10:34:45 +0530

    modified changes

--- src/App.jsx
+++ src/App.jsx
@@ -4,20 +4,26 @@ import QuizzCard from './components/QuizzCard'
 import UserChoice from './components/UserChoice'
 import { quizDataContext } from './ContextApi'
 import { Route, Routes } from 'react-router-dom'
+import SubmitQuiz from './components/SubmitQuiz'
 
 function App() {
-  const [isQuizStart, setIsQuizStart] = useState(false)
-  const {quizData} = useContext(quizDataContext)
+
+  const { isQuizStart, setIsQuizStart, isSubmitQuiz, setIsSubmitQuiz } = useContext(quizDataContext)
   return (
     <>
       <div className=' w-full min-h-screen flex justify-center items-center bg-gray-900'>
         {
-          isQuizStart ?(
-            <QuizzCard/>
+          isQuizStart ? (
+            isSubmitQuiz ? (
+              <SubmitQuiz />
+            ) : (
+              <QuizzCard onSubmit={() => setIsSubmitQuiz(true)} />
+            )
           ) : (
-            <UserChoice onStart={() => setIsQuizStart(true)}/>
+            <UserChoice onStart={() => setIsQuizStart(true)} />
           )
         }
+        {/* <SubmitQuiz/> */}
       </div>
     </>
   )
