5b02ffa3a841c0950094ad5ce481c0a906d2e441
Yashmevada2807 <yashmevada07@gmail.com>
2025-08-24 00:34:47 +0530

    add formData handler feature in reducer

--- src/features/quizz/quizzSlice.js
+++ src/features/quizz/quizzSlice.js
@@ -1,23 +1,28 @@
 import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
 import axios from "axios";
 
-export const fetchedQuizData = createAsyncThunk('quiz/fetchquizdata', async ({ amount, type, difficulty }) => {
-
+export const fetchedQuizData = createAsyncThunk('/quiz/fetchquizdata', async ({ amount, type, difficulty }) => {
     const response = await axios.get('https://opentdb.com/api.php?', {
         params: {
             amount: amount,
-            type: type,
             difficulty: difficulty,
+            type: type,
         }
     })
-    return response.data()
+    return response.data
 })
 
 export const quizSlice = createSlice({
     name: 'quiz',
     initialState: {
+        formData: {
+            amount: 10,
+            difficulty: 'easy',
+            type: 'multiple',
+        },
         quizData: [],
         isLoading: false,
+        status: null,
         score: 0,
         correctAnswer: 0,
         incorrectAnswer: 0,
@@ -31,30 +36,40 @@ export const quizSlice = createSlice({
         // Array(quizData.length).fill(null)
     },
     reducers: {
+        setFormData: (state, action) => {
+            const {name, value} = action.payload
+            state.formData[name] = value
+            state.formData[name] = value
+            state.formData[name] = value
+        },
         setScore: (state) => state.score += 1,
         setCorrectAnswer: (state) => state.correctAnswer += 1,
         setIncorrectAnswer: (state) => state.incorrectAnswer += 1,
         setSkippedAnswer: (state) => state.skippedAnswer += 1,
         setCrrIndex: (state) => state.crrIndex += 1,
-        setQuestionInterval: (state, action) => {},
-        setUserAnswer: (state, action) => {}
+        setQuestionInterval: (state, action) => { },
+        setUserAnswer: (state, action) => { }
     },
     extraReducers: (builder) => {
         builder
             .addCase(fetchedQuizData.pending, (state) => {
                 state.isLoading = true
+                state.status = 'pending'
             })
             .addCase(fetchedQuizData.fulfilled, (state, action) => {
                 state.isLoading = false
-                state.quizData.push(...action.payload.quizData)
+                state.status = 'success'
+                state.quizData = action.payload.results
+                state.isQuizStart = true
             })
             .addCase(fetchedQuizData.rejected, (state, action) => {
                 state.errors = action.error?.message || 'Something went wrong'
+                state.status = 'rejected'
                 state.isLoading = false
             })
     }
 })
 
-export const { setQuizData, setIsLoading, setScore, setCorrectAnswer, setIncorrectAnswer, setSkippedAnswer, setCrrIdx, setQuestionInterval, setIsModal, setIsSubmitQuiz, setisQuizStart, setUserAnswer } = quizSlice.actions
+export const { setFormData } = quizSlice.actions
 
 export const quizReducer = quizSlice.reducer
