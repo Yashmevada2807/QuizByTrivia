87c0f1545d78e2ce6b266d4b00455d0ce237510a
Yashmevada2807 <yashmevada07@gmail.com>
2025-08-27 01:35:13 +0530

    added redux persistState

--- src/app/store.js
+++ src/app/store.js
@@ -1,9 +1,20 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { combineReducers, configureStore } from "@reduxjs/toolkit";
 import { quizReducer } from "../features/quizz/quizzSlice";
+import { persistStore, persistReducer } from "redux-persist";
+import storage from 'redux-persist/lib/storage'
 
+
+const quizPersistConfig = {
+    key: 'quiz',
+    storage,
+}
+
+const rootReducer = combineReducers({
+    quiz: persistReducer(quizPersistConfig, quizReducer)
+})
 
 export const store = configureStore({
-    reducer : {
-       quiz: quizReducer
-    }
+    reducer : rootReducer
 })
+
+export const persistor = persistStore(store)
