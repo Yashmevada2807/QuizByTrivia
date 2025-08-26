49118e6af8d77ddfa095ac26741795239fe49bd4
Yashmevada2807 <yashmevada07@gmail.com>
2025-08-27 01:36:46 +0530

    added persistGate and persistor

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,8 @@ import './index.css'
 import App from './App.jsx'
 import ContextApi from './ContextApi.jsx'
 import { BrowserRouter } from 'react-router-dom'
+import { persistor } from './app/store.js'
+import { PersistGate } from 'redux-persist/integration/react'
 import { ToastContainer } from 'react-toastify'
 import { Provider } from 'react-redux'
 import { store } from './app/store.js'
@@ -12,8 +14,10 @@ createRoot(document.getElementById('root')).render(
     <BrowserRouter>
         <ContextApi>
             <Provider store={store}>
+                <PersistGate persistor={persistor}>
                 <ToastContainer />
                 <App />
+                </PersistGate>
             </Provider>
         </ContextApi>
     </BrowserRouter>
